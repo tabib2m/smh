a3f137e71508a6cb248506a56d6ed02d1ac14469
Mohsen Haeri <tabib2m@gmail.com>
2018-07-28 02:58:58 +0430

    added config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$_CONFIG=array(	'siteTitle'=>'سیستم مدیریت محتوای سمح',
+				'siteAddress'=>'http://localhost/smh/',
+				'siteRoot'=>'C:/Program Files (x86)/EasyPHP-Devserver-17/eds-www/smh/',
+				'ftp_host'=>'',
+				'ftp_user'=>'',
+				'ftp_pass'=>'',
+				'db_host'=>'',
+				'db_user'=>'',
+				'db_pass'=>'',
+				'db_name'=>''
+				);
+
+
+?>
