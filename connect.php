b61aa80f74bb1c73e7449817a82ca63ada8b33ef
Mohsen Haeri <tabib2m@gmail.com>
2018-07-31 19:52:06 +0430

    setting the charset of db

--- connect.php
+++ connect.php
@@ -2,6 +2,9 @@
 require_once($path."config.php");
 
 $queryHandle=mysqli_connect($_CONFIG['db_host'],$_CONFIG['db_user'],$_CONFIG['db_pass'],$_CONFIG['db_name']);
+mysqli_set_charset($queryHandle,"utf8");
+
+if(!$queryHandle) echo 'Error Connecting To DB!';
 
 function query($query,$showError=true){
 	global $queryHandle;
