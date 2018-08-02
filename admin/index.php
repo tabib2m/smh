c207f088a48080d8057d19d23586898bf32edb5b
Mohsen Haeri <tabib2m@gmail.com>
2018-08-02 20:17:23 +0430

    using breadcrumb

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,12 @@ $path='../';
 require_once($path.'connect.php');
 require_once($path.'admin/checkLogin.php');
 
+$pageTitle='فهرست مدیریت';
+$breadcrumb=array(	$path.'admin/'=>'فهرست مدیریت',
+				  	$path.'admin/test'=>'بخش تستی',
+					
+					);
+
 include('header.php');
 
 
